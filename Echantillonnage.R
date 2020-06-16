2b1546f6fa5f31651dd5520be3e477adf1bc5d8c
raphaelquercia <raphael.quercia@gmail.com>
2020-06-16 14:09:58 +0200

    On rajoute les nouvelles features, on change les types, on supprime les vrais NA et on sample

--- Echantillonnage.R
+++ Echantillonnage.R
@@ -6,49 +6,55 @@ library(caret)
 library(randomForest)
 library(ranger)
 library(data.table)
-
+library(VIM)
 
 ## GET prepared dataset ###
 # data <- data.table::fread(file="taxiTrainDataSet.csv",sep=";",header=TRUE)
-data <- read_rds("data/nycTaxiTrainDataSet.rds")
+taxi <- read_rds("data/nycTaxiTrainDataSet.rds")
 
 ##Transform data type
-num_cols<-c("distance","maxTemp","maxWind","maxPrecip","pickup_longitude","pickup_latitude","dropoff_longitude", "dropoff_latitude", "pickupNbEvents", "dropoffNbEvents", "pickupNbCrashesbyHour", "pickupNbCrashesbyDay", "dropoffNbCrashesbyHour", "dropoffNbCrashesbyDay", "pickupNbBusDelayedHeavyTrafficbyHour", "dropoffNbBusDelayedHeavyTrafficbyHour")
+num_cols<-c("distance", "pickup_longitude","pickup_latitude","dropoff_longitude", "dropoff_latitude", "pickupNbEvents", "dropoffNbEvents", "pickupNbCrashesbyHour", "dropoffNbCrashesbyHour", "pickupNbBusDelayedHeavyTrafficbyHour", "dropoffNbBusDelayedHeavyTrafficbyHour", "maxTemp", "maxWind", "maxPrecip")
 #data[,num_cols] = sapply(data[,num_cols], as.numeric)
-data <- data %>% mutate_at(.vars = vars(num_cols),.funs = as.numeric)
-fact_cols = c("month","weekDay", "pickupHour","dropoffHour","pickupBcode","pickupZcta","dropoffBcode","dropoffZcta", "isRain", "isFromBoroughHeavyTraffic", "isToBoroughHeavyTraffic")
+taxi <- taxi %>% mutate_at(.vars = vars(num_cols),.funs = as.numeric)
+fact_cols = c("month","weekDay", "pickupHour","dropoffHour","pickupBcode", "pickupBorough", "pickupNeighborhood", "pickupZcta","dropoffBcode","dropoffBorough","dropoffNeighborhood","dropoffZcta")
 #data[,fact_cols] = lapply(data[,fact_cols], factor)
-data <- data %>% mutate_at(.vars = vars(fact_cols),.funs = as.factor)
+taxi <- taxi %>% mutate_at(.vars = vars(fact_cols),.funs = as.factor)
 
 
 ##Outliers
-resultat <- boxplot(data[,"trip_duration"],range = 10, outline = FALSE,plot=FALSE)
+resultat <- boxplot(taxi[,"trip_duration"],range = 10, outline = FALSE,plot=FALSE)
 valeurs_aberrantes <- resultat$out
-data <- data[data$trip_duration<min(valeurs_aberrantes),]
+taxi <- taxi[taxi$trip_duration<min(valeurs_aberrantes),]
 
 
+# Rajout de la variable isRain de type boolean = Est ce qu'il est tombé de la pluie à l'heure considérée ?
+# taxi <- mutate(taxi, maxPrecip = as.numeric(maxPrecip))
+# taxi <- mutate(taxi,
+#              isRain = ifelse(maxPrecip > 0, TRUE, FALSE))
+
+# Rajout de la variable isHeavyTraffic
+#  
 ### Main features
 features  <- c("month","weekDay", "pickupHour","pickupNeighborhood","dropoffNeighborhood","distance")
-# Ajout de nouvelles features ?????? 
-### "pickupNbEvents", "dropoffNbEvents"
-### "pickupNbCrashesbyHour", "pickupNbCrashesbyDay", "dropoffNbCrashesbyHour", "dropoffNbCrashesbyDay"
-### "pickupNbBusDelayedHeavyTrafficbyHour", "dropoffNbBusDelayedHeavyTrafficbyHour")
-### "isRain"
-### "isFromBoroughHeavyTraffic", "isToBoroughHeavyTraffic"
+
 
 ##Handle NA
 #plot summary
-library(VIM)
-analysis_na<-summary(aggr(data,sortVar=TRUE))$combinations
+analysis_na<-summary(aggr(taxi,sortVar=TRUE))$combinations
 
 #Remove lines if NA in one of the columns we're interedted in
-lines_to_keep <- complete.cases(data[,c(features,"trip_duration")])
-data <- data[lines_to_keep,]
-
+# Variables sorted by number of missings: 
+#   Variable        Count
+# maxPrecip 1.245368e-01
+# maxWind 7.322668e-02
+# maxTemp 4.972556e-05
+lines_to_keep <- complete.cases(taxi)
+taxi <- taxi[lines_to_keep,]
 
 ##### Echantillonnage
 ratio <- 0.1
-nb_lines <- floor(nrow(data)*ratio)
-data_sampled <- data[sample(nrow(data))[1:nb_lines],]
+nb_lines <- floor(nrow(taxi)*ratio)
+data_sampled <- taxi[sample(nrow(taxi))[1:nb_lines],]
 
-save(data_sampled,file = 'data/data_sampled.rdata')
+##### Sauvegarde
+save(data_sampled,file = 'data/data_sampled3.rdata')
