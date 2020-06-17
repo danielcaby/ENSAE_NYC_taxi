d05b9056b0bbb68268f7b5b8941dc49ac7703cbb
danielcaby <66465651+danielcaby@users.noreply.github.com>
2020-06-17 18:58:10 +0200

    Create server.R
    partie server de l'app shiny

---
+++ shinyapp/server.R
@@ -0,0 +1,240 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output, session) {
+######################################################################### 
+   # Partie faite par Raphaël
+#########################################################################
+  
+    # Filtrage en fonction des années/mois/jours sélectionnés
+    filteredCountDataByHour <- reactive({
+        count(taxiDf %>%
+            filter(pickupDay == input$dayInput) %>%
+              filter(pickupMonth == input$monthInput) %>%
+                filter(pickupYear == input$yearInput) %>%
+                  group_by(pickupHour))
+
+    })
+
+    # Histo nb courses / heure pour la journée sélectionnée par l'utilisateur 
+    output$myPlot <- renderPlotly({
+      plot <- plot_ly()
+      axe_x <- list(title = "Hour")
+      axe_y <- list(title = "Nb traj")
+      plot <- layout(plot, title="Nombre de trajets en fonction du jour et de l'heure", xaxis = axe_x, yaxis = axe_y)
+      plot <- add_bars(plot, x=filteredCountDataByHour()$pickupHour, y=filteredCountDataByHour()$n)
+
+    })
+
+  # # TODO : à voir par la suite quand on aura intégré les zipcode départ/arrivée dans le jeu de données
+  #   output$myMap <- renderLeaflet({
+  #   leaflet() %>%
+  #     addProviderTiles(providers$Stamen.TonerLite,
+  #                      options = providerTileOptions(noWrap = TRUE)
+  #     ) %>%
+  #     addPolygons(data=nycShapeFile,
+  #                 fill=TRUE,
+  #                 stroke=TRUE,
+  #                 opacity=1,
+  #                 color="black",
+  #                 weight=1)
+  # })
+    
+    ###############################################################
+    #Partie pour les stats desc vraies
+    ###############################################################
+    
+    #Description durée - histogramme
+    output$hist_duration <- renderPlotly({
+      p <- tds %>%
+        ggplot()+
+        geom_histogram(aes(x=trip_duration))+
+        xlim(c(1,4000))+
+        ggtitle("Nombre de trajets en fonction de leur durée") +
+        xlab("Durée du trajet (en secondes)") + 
+        ylab("Nombre de trajets")
+      
+      p <- ggplotly(p)
+    })
+    
+    #Description durée - boxplot
+    output$bp_duration <- renderPlotly({
+      p <- tds %>%
+        ggplot(aes(y=trip_duration))+
+        geom_boxplot()+
+        ylim(c(0,1750)) +
+        scale_x_discrete(labels=NULL,breaks=NULL)+
+        ggtitle("Boîte à moustaches - durée des trajets")+
+        ylab("Durée du trajet (en secondes)")
+      
+      p <- ggplotly(p)
+    })
+    
+    #Description Distance - histogramme
+    output$hist_dist <- renderPlotly({
+      p <- tds %>%
+        ggplot()+
+        geom_histogram(aes(x=distance))+
+        ggtitle("Nombre de trajets en fonction de la distance parcourue") +
+        xlab("Distance du trajet (en km)") + 
+        ylab("Nombre de trajets")
+      
+      p <- ggplotly(p)
+    })
+    
+    #Description distance - boxplot
+    output$bp_dist <- renderPlotly({
+      p <- tds %>%
+        ggplot(aes(y=distance))+
+        geom_boxplot()+
+        scale_x_discrete(labels=NULL,breaks=NULL)+
+        scale_y_continuous(limits = c(0,10))+
+        ggtitle("Boîte à moustaches - distance des trajets")+
+        ylab("Distance du trajet (en km)")
+      
+      p <- ggplotly(p)
+    })
+    
+    # Lien distance / durée
+    output$link_dist_dur <- renderPlotly({
+      ##Warning too much data points to be plotted => Need to take a subset
+      set.seed(1234)
+      subset <- tds[sample(nrow(tds),5000),]
+      
+      p <- ggplot(subset)+
+        aes(x=distance,y=trip_duration)+
+        geom_point(color="yellow")+
+        geom_smooth(method=lm, se=FALSE,color="black")+
+        ggtitle("Lien entre la distance et la durée des trajets")+
+        ylab("Durée du trajet (en secondes)")+
+        xlab("Distance du trajet (en km)")
+      p <- ggplotly(p)
+    })
+    
+    # Description heures de départ
+    output$hours_dur <- renderPlotly({
+      p <- tds %>%
+        mutate(pickupHour=as.factor(pickupHour))%>%
+        ggplot()+
+        aes(x=pickupHour,y=trip_duration)+
+        geom_boxplot(outlier.size = NULL,outlier.shape = NA)+
+        ylim(0,2000)+
+        ggtitle("Temps de trajet selon l'heure de départ")+
+        ylab("Durée du trajet (en secondes)")+
+        xlab("Heure de départ du trajet")
+      
+      p <- ggplotly(p)
+    })
+
+    # Description heures de départ
+    output$hours_dist <- renderPlotly({
+      p <- tds %>%
+        mutate(pickupHour=as.factor(pickupHour))%>%
+        ggplot()+
+        aes(x=pickupHour,y=distance)+
+        geom_boxplot(outlier.size = NULL,outlier.shape = NA)+
+        ylim(0,15)+
+        ggtitle("Distance du trajet selon l'heure de départ")+
+        ylab("Distance du trajet (en km)")+
+        xlab("Heure de départ du trajet")
+      
+      p <- ggplotly(p)
+    })
+    
+    # Influence heures coeff reg
+    output$hours_reg <- renderPlotly({
+      #On prend l'échantillon à 5k indiv
+      set.seed(1234)
+      subset <- tds[sample(nrow(tds),5000),]
+      #ggplot + ggplotly
+      p <- ggplot(subset) +
+        aes(y=trip_duration, x=distance, col=pickupHour) +
+        geom_point() +
+        geom_smooth(method=lm, se=FALSE) +
+        ggtitle("Impact de l'heure sur le coefficient de la régression linéaire") +
+        ylab("Distance du trajet (en km)")+
+        xlab("Heure de départ du trajet")+
+        labs(col = "Heure de départ")+
+        theme(legend.position="bottom")
+      
+      
+      p <- ggplotly(p)
+    })
+    
+    # description weekdays durée
+    output$wd_dur <- renderPlot({
+      boxplot(trip_duration~weekDay,
+              data=tds,
+              main="Temps de trajet selon le jour de la semaine",
+              outline=FALSE)
+    })
+    
+    # description weekdays distance
+    output$wd_dist <- renderPlot({
+      boxplot(distance~weekDay,
+              data=tds,
+              main="Durée du trajet selon le jour de la semaine",
+              outline=FALSE)
+    })
+    
+    # Influence weekdays coeff reg
+    output$wd_reg <- renderPlotly({
+      #On prend l'échantillon à 5k indiv
+      set.seed(1234)
+      subset <- tds[sample(nrow(tds),5000),]
+      #ggplot + ggplotly
+      p <- ggplot(subset)+
+        aes(y=trip_duration, x=distance, col=weekDay) +
+        geom_point() + 
+        geom_smooth(method=lm, se=FALSE)+
+        ggtitle("Impact du jour de la semaine sur coefficient de la régression linéaire")
+      
+      p <- ggplotly(p)
+    })
+    
+    # On crée la carte avec les polygones
+    output$map_dens <- renderLeaflet({
+      #on définit les coupures (quantiles) et la palette de couleurs 
+      #(impossible de faire marcher leaflet sans palette prédéfinie)
+      bins <- c(0, 3, 20, 316, 731, 2000, Inf)
+      pal <- colorBin("YlOrRd", domain = data_map1$nb_trips, bins = bins)
+      
+      #On définit ce que s'affiche sur la carte
+      labels <- sprintf(
+        "<strong>%s</strong><br/>%g trajets effectués",
+        data_map1$zcta, data_map1$nb_trips
+      ) %>% lapply(htmltools::HTML)
+      
+      m <- leaflet() %>%
+        addProviderTiles(providers$Stamen.TonerLite,
+                         options = providerTileOptions(noWrap = TRUE)
+        ) %>%
+        addPolygons(data=data_map1,
+                    fillColor = ~pal(nb_trips),
+                    weight = 2,
+                    opacity = 1,
+                    color = "black",
+                    dashArray = "3",
+                    fillOpacity = 0.7,
+                    highlight = highlightOptions(
+                      weight = 5,
+                      color = "#666",
+                      dashArray = "",
+                      fillOpacity = 0.7,
+                      bringToFront = TRUE),
+                    label = labels,
+                    labelOptions = labelOptions(
+                      style = list("font-weight" = "normal", padding = "3px 8px"),
+                      textsize = "15px",
+                      direction = "auto")) %>% 
+        addLegend(pal = pal, values = data_map1$nb_trips, opacity = 0.7, title = NULL,
+                   position = "bottomright")
+    })
+})
