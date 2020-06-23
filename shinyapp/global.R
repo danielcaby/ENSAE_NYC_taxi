c3a0c16b6eadfa8e488b1a2e633f583dc91e38cf
danielcaby <66465651+danielcaby@users.noreply.github.com>
2020-06-23 21:32:46 +0200

    Update global.R
    ajout de fonctionalités pour la prediction ou l'exploration

--- shinyapp/global.R
+++ shinyapp/global.R
@@ -2,10 +2,14 @@
 # # # 
 # 
 
+##############################################################################################
+#Packages
+##############################################################################################
+
 #On installe tous les package qui manquent si besoin
 #on vérifie que tous les packages sont bien installés
 list.of.packages <- c("shiny", "dplyr","leaflet","plotly",
-                      "colourpicker","rAmCharts","shinythemes","sf")
+                      "colourpicker","rAmCharts","shinythemes","sf","DT")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)>0) install.packages(new.packages,dependencies = T)
 
@@ -18,40 +22,73 @@ library(colourpicker)
 library(rAmCharts)
 library(shinythemes)
 library(sf)
+library(DT)
 
-
+##############################################################################################
+#chargement des données
+##############################################################################################
 
 # Load RData
-load(file='taxi.RData')
+#load(file='taxi.RData')
+#Echantillon données
 load(file='data_sampled3.rdata')
+#full dataset
 tds <- data_sampled
+full_taxi <- readRDS("data/nycTaxiTrainDataSet.rds") %>%
+  dplyr::mutate(maxTemp = as.numeric(maxTemp),
+                maxWind = as.numeric(maxWind),
+                maxPrecip = as.numeric(maxPrecip))
+#modèle entrainé
+rf_taxi <- readRDS("data/taxiRfModel.rds")
 
-# Création des données pour les cartes
-#Carte 1, on ajoute au nycShapeFile les nb trajets 
-data_map1 <- nycShapeFile %>%
-  left_join(.,
-            tds %>%
-              group_by(pickupZcta)%>%
-              summarise(nb_trips = NROW(vendor_id)),
-            by=c("zcta"="pickupZcta"))
+##############################################################################################
+# Création des données pour l'exploration de data onglet exploration
+##############################################################################################
 
 # Création des structures pour les listes du frontal Rshiny
-yearSelectListChoices <- taxiDf %>%
-  distinct(pickupYear) %>%
-  arrange(pickupYear)
+yearSelectListChoices <- full_taxi %>%
+  distinct(year) %>%
+  arrange(year)
 
 # DEB BUG https://stackoverflow.com/questions/43098849/display-only-one-value-in-selectinput-in-r-shiny-app/54525603#54525603 ==> le nom de la liste est bien crade 
 names(yearSelectListChoices) <- 2016
 # FIN BUG
 
-monthSelectListChoices <- taxiDf %>%
-  distinct(pickupMonth) %>%
-  arrange(pickupMonth)
+monthSelectListChoices <- full_taxi %>%
+  distinct(month) %>%
+  arrange(month)
 
-daySelectListChoices <- taxiDf %>%
-  distinct(pickupDay) %>%
-  arrange(pickupDay)
+daySelectListChoices <- full_taxi %>%
+  distinct(day) %>%
+  arrange(day)
 
-hourSelectListChoices <- taxiDf %>%
+hourSelectListChoices <- full_taxi %>%
   distinct(pickupHour) %>%
   arrange(pickupHour)
+
+zctaSelectListChoices <- full_taxi %>%
+  distinct(pickupZcta) %>%
+  arrange(pickupZcta)
+
+##############################################################################################
+# Création des données pour les cartes
+##############################################################################################
+
+#Carte 1, on ajoute au nycShapeFile les nb trajets 
+#option 1 avec echantillon
+# data_map1 <- nycShapeFile %>%
+#   left_join(.,
+#             tds %>%
+#               dplyr::group_by(pickupZcta)%>%
+#               dplyr::summarise(nb_trips = NROW(vendor_id)),
+#             by=c("zcta"="pickupZcta"))
+
+# option 2 full data
+data_map1_bis <- nycShapeFile %>%
+  left_join(.,
+            full_taxi %>%
+              dplyr::mutate(pickupZcta = as.factor(pickupZcta)) %>%
+              dplyr::group_by(pickupZcta)%>%
+              dplyr::summarise(nb_trips = NROW(vendor_id)) %>%
+              dplyr::ungroup(),
+            by=c("zcta"="pickupZcta"))
